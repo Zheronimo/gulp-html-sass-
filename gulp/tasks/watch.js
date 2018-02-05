ff3ca4de087147b01f112d20df7516171ede8d37
Zheronimo <kirichenko.e.o@gmail.com>
2018-02-05 23:01:00 +0200

    Правки2

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 module.exports = function(){
     $.gulp.task('watch', function(){
         $.gulp.watch('src/html/**/*.html', $.gulp.series('include'));
-        $.gulp.watch('src/static/sass/**/*.{,scss,css}', $.gulp.series('sass:dev'));
+        $.gulp.watch('src/static/sass/**/*.{sass,scss,css}', $.gulp.series('sass:dev'));
         $.gulp.watch('src/static/js/libs/*.js', $.gulp.series('scripts:lib'));
         $.gulp.watch('src/static/img/svg/*.svg', $.gulp.series('svg'));
         $.gulp.watch('src/static/js/main.js', $.gulp.series('scripts'));
